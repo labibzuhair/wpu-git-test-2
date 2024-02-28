3e73511890fce05d17676761f4f41572b1c7c4ea
labib <labiblpc@gmail.com>
2024-02-28 09:23:03 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,12 @@
 </head>
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dolorum, in illum, quibusdam nulla ad excepturi necessitatibus maiores quidem natus mollitia. Dicta, dolorem repellat quod, illum quia architecto officiis earum porro voluptas nemo delectus ut ipsum cum ex incidunt dolore ducimus ratione laboriosam amet ipsam, eaque nulla nobis consectetur harum? Atque vitae, doloremque numquam, quaerat consequuntur ipsam possimus aspernatur placeat tempora harum nesciunt pariatur dolorum fuga reiciendis nisi neque ad enim illo maxime? Voluptatum, accusantium iusto. Quas, hic sunt vitae labore reiciendis, neque ad saepe ipsam dolorum nemo adipisci alias tempora praesentium quidem laborum, animi laudantium explicabo. Fuga facere porro sed laboriosam at quam dicta quo reprehenderit illum nisi? Corporis eos error maxime voluptatibus eum et aperiam rerum nesciunt! Aliquam totam eos iste est sint laboriosam unde impedit odit atque suscipit sed consequuntur provident, officiis accusamus necessitatibus, maxime possimus perspiciatis ipsam! Pariatur magnam tempore error quibusdam atque nihil molestiae sunt, earum cumque aspernatur natus optio nesciunt fugit expedita provident amet ab necessitatibus quis quo id illum illo esse architecto? Ut corrupti alias suscipit, nesciunt quae ipsum accusantium nam, tempora quis deleniti laborum nostrum ad! Deleniti cum labore earum libero doloribus quo. Iure doloribus modi reprehenderit, voluptas doloremque perspiciatis. Dolorem repellat unde iste quis error. Eum necessitatibus illum harum placeat a, eos ullam nihil quibusdam, quasi repellat voluptates. Obcaecati nam dolor, tempore, quasi atque tenetur dolore suscipit necessitatibus esse vero quam quaerat, rem animi placeat modi natus quidem corrupti quae tempora! Perferendis voluptate omnis ea! Tempore earum magni optio nostrum eaque autem quibusdam sunt harum necessitatibus, numquam voluptas saepe doloremque qui iste iusto repellendus esse voluptates itaque illo aliquam consectetur accusantium modi praesentium dolor? Aliquid quia repudiandae atque porro assumenda possimus esse similique tempore cumque optio, eligendi dolorem, consectetur non, ab totam unde corrupti explicabo nostrum animi ea nemo placeat laborum!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+        <li>empat</li>
+        <li>lima</li>
+    </ul>
 </body>
 </html>
